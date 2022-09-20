4007cf88f793e520896d37959e060022bbd54b02
Thomasparsley <info@thomasparsley.cz>
2022-09-20 21:03:06 +0200

    update

---
+++ bachelor/app/sql/consts.py
@@ -0,0 +1,38 @@
+from typing import Final
+
+
+SQL_KEYWORDS: Final = (
+    "SELECT",
+    "FROM",
+    "WHERE",
+    "AND",
+    "OR",
+    "LIKE",
+    "IN",
+    "IS",
+    "NULL",
+    "NOT",
+    "TRUE",
+    "FALSE",
+    "DISTINCT",
+    "ALL",
+    "AS",
+    "ASC",
+    "DESC",
+    "ORDER",
+    "BY",
+    "GROUP",
+    "HAVING",
+    "LIMIT",
+    "OFFSET",
+    "INTO",
+    "VALUES",
+    "DUPLICATE",
+    "KEY",
+    "PRIMARY",
+    "UNIQUE",
+    "CHECK",
+    "CONSTRAINT",
+)
+
+SQL_MACROS: Final = ("SYMDIFF",)
