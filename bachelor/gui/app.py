995f3bbca96b7f8d8f88a8eefd7bf14e692b435b
Tomáš Petržela <info@thomasparsley.cz>
2022-10-04 14:08:00 +0200

    update

--- bachelor/gui/app.py
+++ bachelor/gui/app.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import tkinter as tk
 
 from .frames.repl import REPL
@@ -16,6 +18,8 @@ class App(tk.Tk):
 
         self._init_layout()
 
+        self.bind("<FocusIn>", self._set_default_main_menu_event)
+
     def _init_layout(self) -> None:
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
@@ -28,38 +32,9 @@ class App(tk.Tk):
         self.paned_window.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
 
         self._main_menu()
-        self.config(menu=self._main_menu_bar)
 
     def _main_menu(self):
-        self._main_menu_bar = tk.Menu(self)
-
-        ###########
-        # File menu
-        file_menu = tk.Menu(self._main_menu_bar, tearoff=0)
-        file_menu.add_command(label="Novy", accelerator="Ctrl+N", underline=1)
-        file_menu.add_command(label="Otevrit", accelerator="Ctrl+O", underline=1)
-        file_menu.add_command(label="Ulozit", accelerator="Ctrl+S")
-        file_menu.add_command(label="Ulozit jako", accelerator="Ctrl+Shift+S")
-        file_menu.add_separator()
-        file_menu.add_command(label="Ukoncit", command=self.quit, accelerator="Ctrl+Q")
-
-        ###############
-        # SQL Menu
-        sql_menu = tk.Menu(self._main_menu_bar, tearoff=0)
-        sql_menu.add_command(label="Spustit vše")
-        sql_menu.add_command(label="Spustit vybrané")
-        sql_menu.add_separator()
-        sql_menu.add_command(label="Formátovat", command=self._format_editor)
-
-        ###############
-        # Windows Menu
-        windows = tk.Menu(self._main_menu_bar, tearoff=0)
-        windows.add_command(label="Duplicate editor", command=self._duplicate_editor)
-        windows.add_command(label="Duplicate repl", command=self._duplicate_repl)
-
-        self._main_menu_bar.add_cascade(label="Soubor", menu=file_menu)
-        self._main_menu_bar.add_cascade(label="SQL", menu=sql_menu)
-        self._main_menu_bar.add_cascade(label="Windows", menu=windows)
+        self.set_default_main_menu()
 
     def _editor_init(self) -> None:
         self._editor = Editor(self)
@@ -79,3 +54,46 @@ class App(tk.Tk):
 
     def _format_editor(self):
         self._editor.format()
+
+    def set_main_menu(self, menu: tk.Menu):
+        if hasattr(self, "_main_menu_bar"):
+            self._main_menu_bar.destroy()
+
+        self._main_menu_bar = menu
+        self.config(menu=self._main_menu_bar)
+
+    def _set_default_main_menu_event(self, event: Any):
+        self.set_default_main_menu()
+
+    def set_default_main_menu(self):
+        menu = tk.Menu(self)
+
+        ###########
+        # File menu
+        file_menu = tk.Menu(menu, tearoff=0)
+        file_menu.add_command(label="Novy", accelerator="Ctrl+N", underline=1)
+        file_menu.add_command(label="Otevrit", accelerator="Ctrl+O", underline=1)
+        file_menu.add_command(label="Ulozit", accelerator="Ctrl+S")
+        file_menu.add_command(label="Ulozit jako", accelerator="Ctrl+Shift+S")
+        file_menu.add_separator()
+        file_menu.add_command(label="Ukoncit", command=self.quit, accelerator="Ctrl+Q")
+
+        ###############
+        # SQL Menu
+        sql_menu = tk.Menu(menu, tearoff=0)
+        sql_menu.add_command(label="Spustit vše")
+        sql_menu.add_command(label="Spustit vybrané")
+        sql_menu.add_separator()
+        sql_menu.add_command(label="Formátovat", command=self._format_editor)
+
+        ###############
+        # Windows Menu
+        windows = tk.Menu(menu, tearoff=0)
+        windows.add_command(label="Duplicate editor", command=self._duplicate_editor)
+        windows.add_command(label="Duplicate repl", command=self._duplicate_repl)
+
+        menu.add_cascade(label="Soubor", menu=file_menu)
+        menu.add_cascade(label="SQL", menu=sql_menu)
+        menu.add_cascade(label="Windows", menu=windows)
+
+        self.set_main_menu(menu)
--- bachelor/gui/windows/editor_window.py
+++ bachelor/gui/windows/editor_window.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import tkinter as tk
 
 from ..frames.editor import Editor
@@ -5,7 +7,7 @@ from ..widgets.textpeer import TextPeer
 
 
 class EditorWindow:
-    def __init__(self, root: tk.Misc | None, editor_master: tk.Text):
+    def __init__(self, root: tk.Misc, editor_master: tk.Text):
         self.window = tk.Toplevel(root)
         self.window.title("Duplicated editor")
 
@@ -18,19 +20,22 @@ class EditorWindow:
         self.editor.set_peer(editor_master)
         self.editor.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
 
-        # self._main_menu()
+        self._main_menu(root)
 
-    def _main_menu(self):
-        self.main_menu_bar = tk.Menu(self.window)
+    def _main_menu(self, root: tk.Misc):
+        def set_menu(event: Any):
+            menu = tk.Menu(self.window)
 
-        ###############
-        # SQL Menu
-        sql_menu = tk.Menu(self.main_menu_bar, tearoff=0)
-        sql_menu.add_command(label="Spustit vše")
-        sql_menu.add_command(label="Spustit vybrané")
+            ###############
+            # SQL Menu
+            sql_menu = tk.Menu(menu, tearoff=0)
+            sql_menu.add_command(label="Spustit vše")
+            sql_menu.add_command(label="Spustit vybrané")
+            sql_menu.add_separator()
+            sql_menu.add_command(label="Formátovat")
 
-        sql_menu.add_separator()
+            menu.add_cascade(label="SQL", menu=sql_menu)
 
-        sql_menu.add_command(label="Formátovat")
+            root.set_main_menu(menu)  # type: ignore
 
-        self.main_menu_bar.add_cascade(label="SQL", menu=sql_menu)
+        self.window.bind("<FocusIn>", set_menu)
